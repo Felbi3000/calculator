9ff6045f22ed58050eccc2990dbad100fd11f0b0
Felbi3000 <74322100+Felbi3000@users.noreply.github.com>
2025-09-08 14:21:37 +0200

    Finish project
    created rest of functions including clear Button. Text Box is disabled and can only display numbers. Implemented Error handling on dividing by zero.
    Implemented basic calculator layout.
    ToDo: Decimals, Backspace, Keyboard Support

--- script.js
+++ script.js
@@ -20,8 +20,10 @@ function operate (op){
       sum = num1 * num2;
       break;
     case "div":
+       {
       sum = num1 / num2;
       break;
+      };
   }
 }
 
@@ -31,29 +33,49 @@ function operate (op){
 buttons.forEach((button) => {
   // and for each one we add a 'click' listener
   button.addEventListener("click", () => {
+    
     switch (button.id){
-    case "0":
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-      inputField.value = button.id;
-      if (operatePressed === true){num2 = parseFloat(button.id)} else { num1 = parseFloat(button.id);}
-      break;
-    case "operate":
-      operate(operator)
-      inputField.value = sum;
-      operatePressed = false;
-      break;
-    default:
-      operator = button.id;
-      operatePressed = true
-      break
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        if (operatePressed === false){
+          num1 = parseInt (num1.toString().concat(button.id));
+          inputField.value = num1;
+        } else {
+          num2 = parseInt(num2.toString().concat(button.id));
+          inputField.value = num2;
+        };
+        break;
+      case "operate":
+        if (operator === "div" && num2 === 0) {
+          inputField.value = "ERROR";
+          break;
+        } else {
+          operate(operator)
+          num1 = sum;
+          inputField.value = num1;
+          num2 = 0;
+          // operatePressed = false;
+          break;
+        }
+      case "clr":
+        num1 = 0;
+        num2 = 0;
+        sum = 0;
+        operatePressed = false;
+        inputField.value = "0";
+        break;
+      default:
+        operator = button.id;
+        operatePressed = true
+        break;
   }
   });
 });
